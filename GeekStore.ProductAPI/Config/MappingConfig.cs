fb717cbd1788dac758b68c210cbb68b7c3093074
Roberto Frisanco dos Santos <roberto.frisanco@univag.iemat>
2023-11-22 10:54:19 -0400

    fix migrations ( deleted the modelSnapshot and execute add-migration again)

---
+++ GeekStore.ProductAPI/Config/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using GeekStore.ProductAPI.Data.VO;
+using GeekStore.ProductAPI.Model;
+
+namespace GeekStore.ProductAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config => {
+                config.CreateMap<ProductVO, Product>();
+                config.CreateMap<Product, ProductVO>();
+            });
+            return mappingConfig;
+        }
+    }
+}
---
+++ GeekStore.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using GeekStore.Web.Models;
+
+namespace GeekStore.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> GetAllProduct();
+
+        Task<ProductModel> GetProductById(long Id);
+
+        Task<ProductModel> CreateProduct(ProductModel model);
+
+        Task<ProductModel> UpdateProduct(ProductModel model);
+
+        Task<bool> DeleteProductById(long Id);
+    }
+}
